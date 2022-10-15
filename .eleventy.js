5cef552f89e310699421e1d6e1c00faa2912030f
Farai Madzima <farai@f4r4i.local>
2022-10-14 21:43:15 -0400

    added passthru for fonts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget("./src/scss/");
-
+  eleventyConfig.addPassthroughCopy("./src/fonts");
+  eleventyConfig.addPassthroughCopy("./src/favicon.ico");
   return {
     dir: {
       input: "src",
