b9233758ce076862b79f0622f1010299de8aeffb
Farai Madzima <farai@f4r4i.lan>
2022-10-22 10:13:20 -0400

    improved article list layout

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,7 @@
 module.exports = {
   // NOTE: `process.env.URL` is provided by Netlify, and may need
   // adjusted pending your host
-  url: process.env.URL || "http://localhost:8080",
+  siteUrl: process.env.URL || "http://localhost:8080",
   siteName: "heyfarai.com",
   siteDomain: "heyfarai.com",
   siteTitle: "Coach and trainer for people managers",
